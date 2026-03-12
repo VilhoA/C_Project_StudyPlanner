bbf3f383a5c9eaba42a98db55421f8e681fde8b0
ville <ville.ahola5@metropolia.fi>
2026-03-12 18:19:02 +0200

    Fixed build error by adding stdbool

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include "schedule.h"
+#include <stdbool.h>
 
 int remove_linefeed(char *string);
 int find_classes_by_day(ClassSession all_classes[], int total_count, const char *target_day, ClassSession found_classes[]);
